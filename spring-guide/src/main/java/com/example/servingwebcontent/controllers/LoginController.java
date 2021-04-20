c15a851a476cdc98ebf55e2a1673a2fddcc36db2
Carlos Pomares <cpomaresp@gmail.com>
2021-04-20 16:54:06 +0200

    Added JPA

--- spring-guide/src/main/java/com/example/servingwebcontent/controllers/LoginController.java
+++ spring-guide/src/main/java/com/example/servingwebcontent/controllers/LoginController.java
@@ -3,7 +3,6 @@ package com.example.servingwebcontent.controllers;
 import com.example.servingwebcontent.representation.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
